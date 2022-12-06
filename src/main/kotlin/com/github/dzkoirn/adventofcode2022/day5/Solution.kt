11064c67301828e423fa706b41ccfabea024b1f2
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2022-12-07 00:27:13 +0100

    Solved day 5 puzzle

--- src/main/kotlin/com/github/dzkoirn/adventofcode2022/day5/Solution.kt
+++ src/main/kotlin/com/github/dzkoirn/adventofcode2022/day5/Solution.kt
@@ -4,7 +4,27 @@ import com.github.dzkoirn.adventofcode2022.readInput
 
 fun main() {
     val lines = readInput("day_5_input")
+    println("Start")
+    println(puzzle1(lines))
+    println("-------------")
+    println(puzzle2(lines))
+    println("Finish")
 }
+
+fun puzzle1(lines: List<String>): String {
+    val input = parseInput(lines)
+    val result = executeCommands(input)
+    return result.convertToResultString()
+}
+
+fun puzzle2(lines: List<String>): String {
+    val input = parseInput(lines)
+    val result = executeCommands2(input)
+    return result.convertToResultString()
+}
+
+fun List<List<Char>>.convertToResultString() =
+    this.map { it.last() }.joinToString(separator = "")
 
 data class Input(
     val stacks: List<List<Char>>,
@@ -63,4 +83,25 @@ fun parseCommands(lines: List<String>): List<Input.Command> {
             from = it[3].toInt(),
             to = it[5].toInt()
         ) }
+}
+
+fun executeCommands(input: Input): List<List<Char>> {
+    return input.commands.fold(input.stacks.map { it.toMutableList() }) { acc, command ->
+        repeat(command.count) {
+           val e = acc[command.from - 1].removeLast()
+           acc[command.to - 1].add(e)
+        }
+        acc
+    }
+}
+
+fun executeCommands2(input: Input): List<List<Char>> {
+    return input.commands.fold(input.stacks.map { it.toMutableList() }) { acc, command ->
+        val movingItems = acc[command.from - 1].takeLast(command.count)
+        acc[command.to - 1].addAll(movingItems)
+        repeat(command.count) {
+            acc[command.from - 1].removeLast()
+        }
+        acc
+    }
 }
--- src/test/kotlin/com/github/dzkoirn/adventofcode2022/day5/SolutionTest.kt
+++ src/test/kotlin/com/github/dzkoirn/adventofcode2022/day5/SolutionTest.kt
@@ -16,6 +16,32 @@ class SolutionTest {
         assertEquals(expectedCommands, result.commands)
     }
 
+    @Test
+    fun testExecuteCommand() {
+        val input = parseInput(example_input.lines())
+        val result = executeCommands(input)
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun testPuzzle1() {
+        val result = puzzle1(example_input.lines())
+        assertEquals(expectedPuzzle1Result, result)
+    }
+
+    @Test
+    fun testExecuteCommand2() {
+        val input = parseInput(example_input.lines())
+        val result = executeCommands2(input)
+        assertEquals(expectedResult2, result)
+    }
+
+    @Test
+    fun testPuzzle2() {
+        val result = puzzle2(example_input.lines())
+        assertEquals(expectedPuzzle2Result, result)
+    }
+
     companion object {
 
         val example_input = """
@@ -42,5 +68,37 @@ class SolutionTest {
             Input.Command(2, 2, 1),
             Input.Command(1, 1, 2)
         )
+
+        /**
+         *         [Z]
+         *         [N]
+         *         [D]
+         * [C] [M] [P]
+         *  1   2   3
+         */
+        val expectedResult =
+            listOf(
+                listOf('C'),
+                listOf('M'),
+                listOf('P', 'D', 'N', 'Z')
+            )
+
+        val expectedPuzzle1Result = "CMZ"
+
+        /**
+         *         [D]
+         *         [N]
+         *         [Z]
+         * [M] [C] [P]
+         *  1   2   3
+         */
+        val expectedResult2 =
+            listOf(
+                listOf('M'),
+                listOf('C'),
+                listOf('P', 'Z', 'N', 'D')
+            )
+
+        val expectedPuzzle2Result = "MCD"
     }
 }
