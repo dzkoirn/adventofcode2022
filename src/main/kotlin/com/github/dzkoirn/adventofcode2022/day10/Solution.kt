610649711bb0067a2c9373d7b600ee2559f66670
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2022-12-14 09:52:44 +0100

    Solved day 10 first part

---
+++ src/main/kotlin/com/github/dzkoirn/adventofcode2022/day10/Solution.kt
@@ -0,0 +1,91 @@
+package com.github.dzkoirn.adventofcode2022.day10
+
+import com.github.dzkoirn.adventofcode2022.readInput
+import java.util.LinkedList
+import java.util.Queue
+
+fun main() {
+    val input = readInput("day_10_input")
+    println("Start")
+    println("Puzzle 1")
+    val result = getValuesOnSpecificTicks(input, defaultTicksToCare)
+        .calculateSignalStrength()
+    println(result)
+    println("===================")
+    println("Puzzle 1")
+    println("Finish")
+}
+
+sealed class Command {
+    object Noop : Command()
+    data class Add(val value: Int) : Command()
+}
+
+fun Command.asAdd() = this as Command.Add
+
+fun parseInput(input: List<String>): Queue<Command> =
+    input.map {
+        if (it.startsWith("noop")) {
+            Command.Noop
+        } else {
+            Command.Add(it.split(" ")[1].toInt())
+        }
+    }.let { LinkedList(it) }
+
+fun execute(commands: Queue<Command>, onTick: (Long, Int) -> Unit = { _, _ -> }): Int {
+    var register = 1
+    var tick = 0L
+    val pipeline = LinkedList<Command>()
+
+    do {
+        val command = if (pipeline.isNotEmpty()) {
+            null
+        } else if (commands.isEmpty()) {
+            return register
+        } else {
+            commands.poll()
+        }
+        onTick(++tick, register)
+        if (command == null) {
+            register += pipeline.poll().asAdd().value
+        } else if (command is Command.Add) {
+            pipeline.add(command)
+        }
+    } while (true)
+}
+
+val defaultTicksToCare = arrayOf(20L, 60L, 100L, 140L, 180L, 220L)
+
+fun getValuesOnSpecificTicks(
+    input: List<String>,
+    ticksToCare: Array<Long> = defaultTicksToCare
+): List<Pair<Long, Int>> {
+    val collector = TicksValueCollector(ticksToCare, )
+    execute(parseInput(input), collector::onTick)
+    return collector.getValues()
+}
+
+fun List<Pair<Long, Int>>.calculateSignalStrength() =
+    sumOf { (tick, value) -> tick * value }
+
+class TicksValueCollector(
+    private val ticksToCare : Array<Long>,
+) {
+    private val values = mutableListOf<Pair<Long, Int>>()
+    fun onTick(tick: Long, value: Int) {
+        if (tick in ticksToCare) {
+            values.add(Pair(tick, value))
+        }
+    }
+
+    fun getValues(): List<Pair<Long, Int>> = values
+}
+
+class EverythingCollector {
+    private val values = mutableListOf<Pair<Long, Int>>()
+    fun onTick(tick: Long, value: Int) {
+        values.add(Pair(tick, value))
+    }
+
+    fun getValues(): List<Pair<Long, Int>> = values
+}
---
+++ src/test/kotlin/com/github/dzkoirn/adventofcode2022/day10/SolutionTest.kt
@@ -0,0 +1,199 @@
+package com.github.dzkoirn.adventofcode2022.day10
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+
+    @Test
+    fun testSmallInputExample() {
+        val collector = EverythingCollector()
+        val endValue = execute(parseInput(smallInputExample.lines()), collector::onTick)
+        assertEquals(smallInputExpectedOutput, collector.getValues())
+        assertEquals(-1, endValue)
+    }
+
+    @Test
+    fun testValuesOnTicks() {
+        val result = getValuesOnSpecificTicks(exampleInput.lines(), ticksToCare)
+            .map { (_, value) -> value }
+        assertEquals(expectedValuesOnTicks, result)
+    }
+
+    @Test
+    fun testSignalStrengthCalculation() {
+        val result = getValuesOnSpecificTicks(exampleInput.lines(), ticksToCare)
+            .calculateSignalStrength()
+        assertEquals(expectedSignalStrength, result)
+    }
+
+    companion object {
+        val smallInputExample = """
+            noop
+            addx 3
+            addx -5
+        """.trimIndent()
+
+        val smallInputExpectedOutput =
+            listOf(
+                Pair(1L, 1),
+                Pair(2L, 1),
+                Pair(3L, 1),
+                Pair(4L, 4),
+                Pair(5L, 4),
+            )
+
+        val exampleInput = """
+            addx 15
+            addx -11
+            addx 6
+            addx -3
+            addx 5
+            addx -1
+            addx -8
+            addx 13
+            addx 4
+            noop
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
+        """.trimIndent()
+
+        val ticksToCare = arrayOf(20L, 60L, 100L, 140L, 180L, 220L)
+        val expectedValuesOnTicks = listOf(21, 19, 18, 21, 16, 18)
+        const val expectedSignalStrength = 13140L
+    }
+}
